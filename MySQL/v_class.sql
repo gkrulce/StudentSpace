0a8485b2277952fff881def7096cd6030ff9dfc0
Galen Krulce <gkrulce@ucsd.edu>
2015-04-02 05:35:12 +0000

    Modified class view to remove repetition bug

--- MySQL/v_class.sql
+++ MySQL/v_class.sql
@@ -2,7 +2,5 @@ USE StudyTree;
 DROP VIEW IF EXISTS v_class;
 
 CREATE VIEW v_class AS
-SELECT c.id, CONCAT(d.short_name, " ", c.number) name FROM users u
-	join users_to_classes uc on u.id=uc.user_id
-	join classes c on c.id = uc.class_id
-	join departments d on d.id = c.department_id
+SELECT c.id, CONCAT(d.short_name, " ", c.number) name FROM classes c 
+	JOIN departments d on d.id = c.department_id
