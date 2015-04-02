fb1727ed1ebdb490ca40efd0a105bbf43848a292
Galen Krulce <gkrulce@ucsd.edu>
2015-04-02 19:07:29 +0000

    Added code for login and session ids to make testing more natural

--- PHP/Student.php
+++ PHP/Student.php
@@ -1,36 +1,18 @@
 <?php
-include('Section.php');
 class Student
 { 
   private $uid;
   private $user_name;
-  private $db;
-  private $classes = array();
 
-  public function __construct($uid, $db)
+  public function __construct($uid, $user_name)
   {
-    $this->db = $db;
     $this->uid = $uid;
-
-    //Fetches classes into $classes array
-    $result = $this->db->query('SELECT c.id, c.name FROM users u JOIN users_to_classes uc ON u.id=uc.user_id JOIN v_class c ON c.id=uc.class_id WHERE u.id='. $this->uid.';');
-    
-    foreach($result as $row)
-    {
-      $this->classes[] = new Section($row["id"], $row["name"]);
-    }
+    $this->user_name = $user_name;
   }
 
-  public function getClasses()
+  public function getClasses($db)
   {
-    return $this->classes;
+    return $db->query('SELECT c.id, c.name FROM users u JOIN users_to_classes uc ON u.id=uc.user_id JOIN v_class c ON c.id=uc.class_id WHERE u.id='. $this->uid.';');
   }
-}
-
-include('db.php');
-$stud = new Student(2, $db);
-foreach($stud->getClasses() as $row)
-{
-  echo $row->getName() . "\n";
 }
 ?>
---
+++ PHP/login.php
@@ -0,0 +1,18 @@
+<?php
+  include('db.php');
+  include('Student.php');
+  $username=$_POST['user_name'];
+  
+  $stmt = $db->prepare("SELECT * FROM users where user_name = :userName;");
+  $stmt->bindParam(":userName", $username, PDO::PARAM_STR);
+  $stmt->execute();
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if($row)
+  {
+    session_start();
+    $_SESSION['user'] = new Student($row["id"], $row["user_name"]);
+    header('Location: view.html');
+  }
+  
+?>
