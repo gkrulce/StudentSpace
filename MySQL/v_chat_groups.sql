3a0ed271c26f05e0a2c0c88a83827934ef0165fc
Galen Krulce <gkrulce@ucsd.edu>
2015-04-17 20:35:26 +0000

    Will no longer hold the uuid to a foreign key constraint

--- MySQL/v_chat_groups.sql
+++
@@ -1,9 +0,0 @@
-USE StudyTree;
-DROP VIEW IF EXISTS v_chat_groups;
-
-CREATE VIEW v_chat_groups AS
-
-SELECT short_desc as name, uuid 
-	FROM study_groups 
-		UNION 
-SELECT name, uuid FROM v_class;
