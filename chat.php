1d4c917a80dfaac7c39206eab05e22b5d859279d
RN Lee <rnlee0054@gmail.com>
2015-05-30 06:13:51 +0000

    Chat looks better, displays group info, anon vs. user name, etc.

--- chat.php
+++ chat.php
@@ -23,7 +23,14 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/chat.css">
     <!-- Angular JS -->
+<!--
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-sanitize.js"></script>
+    <!-- Moment.js for time formatting -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
     <!-- Socket IO JavaScript -->
     <script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>
 
@@ -59,7 +66,8 @@
       <a href="#" data-activates="slide-out" class="button-collapse">
         <i class="mdi-navigation-menu"></i>
       </a>
-      <a href="#" class="brand-logo">Space</a>
+      <a href="#" class="brand-logo">space<span class="amber-text text-darken-1">@ucsd</span></a>
+      <a class="curr-chat">{{rooms[currTab].group_name}}</span></a>
       <!-- Top header-->
       <ul class="navbar right hide-on-med-and-down">
         <li><a href="view.php">View</a></li>
@@ -67,50 +75,78 @@
         <li class="active"><a href="chat.php">My Spaces</a></li>
         <li><a href="settings.php">Settings</a></li>
         <li><a class="blue-text text-lighten-1" href="feedback.php">Feedback</a></li>
+        <li><a href="logout.php">Logout</a></li>
+<!--
+        <li><a class="blue-text text-lighten-1" ng-click="tellMeStuff()">TELLMESTUFF</a></li>
+-->
       </ul>
 
       <!-- Fixed side nav for large screens -->
       <ul id="class-nav" class="side-nav fixed right hide-on-med-and-down">
         <li ng-repeat="r in rooms" role="presentation">
-          <a href="" class="my-tab" data-toggle="tab"
-              ng-click="scrollDownChat($index); switchActiveTab($index)">
+          <a class="my-tab" href="#{{$index}}"
+              ng-click="switchActiveTab($index)">
             {{r.group_name}}
-            <div class="btn class remove red darken-2 circle"
-                ng-click="leaveGroup($index)">
-              <i class="mdi-content-clear small"></i>
+            <div class="btn class grey lighten-2 grey-text text-darken-1"
+                ng-if="r.is_study_group"
+                ng-click="toggleDropdown(r.group_name, $event)">
+              <i class="mdi-navigation-more-vert"></i>
             </div>
-            <div class="btn class info blue accent-3 circle"
-                ng-click="($index)">
-              <i class="mdi-action-info-outline small"></i>
+            <div class="btn class blue accent-3"
+                ng-if="r.is_study_group"
+                ng-click="toggleClassInfo(r.group_name, $event)">
+              <i class="mdi-action-info-outline"></i>
             </div>
           </a>
+
+          <!-- Dropdown -->
+          <ul class="sp-dropdown white grey-text text-darken-4"
+              ng-class="{'sp-dropdown--open': openedDropdownGroup == r.group_name}"
+              ng-show="openedDropdownGroup == r.group_name">
+            <li ng-click="leaveGroup($index)">Leave {{r.group_name}}</li>
+          </ul> <!-- End dropdown -->
         </li>
       </ul>
 
       <!-- Moving side nav for small screens -->
-      <ul id="slide-out" class="side-nav">
+      <ul id="slide-out" class="side-nav"
+          ng-class="{'active': isTabActive($index)}">
+        <li class="blue darken-4 side-nav-backout"
+            ng-click="closeSideNav()">
+          <i class="white-text mdi-navigation-arrow-back medium"></i>
+        </li>
         <!-- Standard links -->
         <li><a href="view.php">View</a></li>
         <li><a href="create.php">Create</a></li>
         <li class="active"><a href="chat.php">Chat</a></li>
         <li><a href="settings.php">Settings</a></li>
         <li><a href="feedback.php">Feedback</a></li>
+        <li><a href="logout.php">Logout</a></li>
 
         <!-- Rooms -->
         <li class="blue accent-3">My Rooms</li>
         <li ng-repeat="r in rooms" role="presentation">
-          <a href="" class="my-tab" data-toggle="tab" ng-click="scrollDownChat();
-              switchActiveTab($index)">
+          <a class="my-tab" href="#{{$index}}"
+              ng-click="switchActiveTab($index)">
             {{r.group_name}}
-            <div class="btn class remove red darken-2 circle"
-                ng-click="leaveGroup($index)">
-              <i class="mdi-content-clear small"></i>
+            <div class="btn class grey lighten-3 grey-text text-darken-1"
+                ng-if="r.is_study_group"
+                ng-click="toggleDropdown(r.group_name, $event)">
+              <i class="mdi-navigation-more-vert small"></i>
             </div>
-            <div class="btn class info blue accent-3 circle"
-                ng-click="($index)">
+            <div class="btn class blue accent-3"
+                ng-if="r.is_study_group"
+                ng-click="toggleClassInfo(r.group_name, $event)">
               <i class="mdi-action-info-outline small"></i>
             </div>
           </a>
+
+          <!-- Dropdown -->
+          <ul class="sp-dropdown white grey-text text-darken-4"
+              ng-class="{'sp-dropdown--open': openedDropdownGroup == r.group_name}"
+              ng-show="openedDropdownGroup == r.group_name">
+            <li ng-click="leaveGroup($index)">Leave {{r.group_name}}</li>
+          </ul> <!-- End dropdown -->
         </li>
       </ul>
     </nav>
@@ -118,20 +154,44 @@
     <!-- Main pane -->
     <div class="chat-area">
       <!-- Holds the messages-->
-      <div id="message-pane">
+      <div class="message-pane">
         <div class="tab-content">
           <div ng-repeat="r in rooms" class="tab-pane" id="tab{{$index}}"
               ng-if="isTabActive($index)">
             <div class="chat-block" ng-repeat="m in r.messages">
-              <a class="btn tooltipped username circle amber darken-1"
-                  data-position="left" data-delay="50" data-tooltip="{{m.username}}"
-                  title="{{m.username}}"
-                  ng-if="m.username != username">
-                {{m.username[0].toUpperCase()}}
-              </a>
-              <div ng-class="{'sent blue accent-3': m.username == username,
-                  'arrive grey lighten-3': m.username != username}" class="chat-bubble">
-                {{m.message}}
+              <div class="chat-row">
+                <div class="grey-text text-lighten-1 chat-sig ani--grow"
+                    ng-if="openedChatSig == $index">
+                  {{m.username}} on {{m.time | dateify_time}}
+                </div>
+                <a class="username circle amber darken-1 btn tooltipped"
+                    data-position="left" data-delay="50" data-tooltip="{{m.username}}"
+                    title="{{m.username}}"
+                    ng-if="m.username != username">
+                  {{m.username[0].toUpperCase()}}
+                </a>
+                <div ng-class="{'chat-bubble--open-sig': openedChatSig == $index, 
+                    'sent blue accent-3': m.username == username,
+                    'arrive grey lighten-3': m.username != username}"
+                    class="chat-bubble"
+                    ng-click="toggleChatSig($index)">
+                  {{m.message}}
+                </div>
+              </div>
+            </div>
+
+            <div class="chat-area--class-info ani--side-in"
+                ng-if="openedClassInfo == r.group_name">
+              <div class="btn waves-effect waves-dark grey lighten-3 grey-text text-darken-2"
+                  ng-click="toggleClassInfo(r.group_name, $event)">
+                <i class="mdi-action-highlight-remove"></i></div>
+              <div class="class-head-card">
+                <h1>{{r.group_name}}</h1>
+                <div><h3 class="grey-text text-darken-1">
+                  {{r.date | dateify_date}} | {{r.time}}</h3></div>
+              </div>
+              <div class="my-body">
+                <p ng-bind-html="r.long_desc"></p>
               </div>
             </div>
           </div>
@@ -141,25 +201,21 @@
       <!-- Chat input box -->
       <form class="input-box" onsubmit="chatReset(); return false;" ng-submit="sendMessage()" action="#">
         <div class="input-field">
-          <input class="resetOnSubmit" type="text" ng-init="inputText = ''"
-              ng-model="inputText" placeholder="Enter a message..."/>
-          <a ng-init="isAnonymous = false" ng-click="isAnonymous = !isAnonymous"
+          <input id="input--message" class="resetOnSubmit" type="text"
+              ng-init="inputText = ''" ng-model="inputText"
+              placeholder="Sending message as {{isAnonymous ? 'anonymous' : username}}..."/>
+          <a ng-init="isAnonymous = false"
+              ng-click="isAnonymous = !isAnonymous"
               ng-class="{'grey-text grey-lighten-3': isAnonymous}"
-              title="Become {{!isAnonymous ? 'anonymous' : 'visible as ' +  username}}"
+              title="Become {{isAnonymous ? 'visible as ' +  username : 'anonymous'}}"
               class="btn sender blue-text blue-accent-3">
             <i class="mdi-action-visibility small"></i>
           </a>
-          <button type="submit" class="btn sender blue-text text-accent-3">
+          <button type="submit" class="btn sender blue-text text-accent-3"
+              ng-click="focusInput()">
             <i class="mdi-content-send small"></i>
           </button>
-          <div class="anon-box">
-            <!--
-            <p>
-              <input type="checkbox" ng-model="isAnonymous" id="anon-check" />
-              <label for="anon-check"></label>
-            </p>
--->
-          </div>
+
         </div>
       </form>
     </div> <!-- End of .chat-area -->
--- js/chat.js
+++ js/chat.js
@@ -3,25 +3,17 @@ function chatReset() {
 }
 var socket = io(":3000");
 
+/*---------------------------------------------------------------------------*/
 /* ANGULAR JS */
-angular.module('ChatApp', ['spaceFilters']).
+/*---------------------------------------------------------------------------*/
+angular.module('ChatApp', ['ngAnimate', 'ngSanitize']).
 
-controller('ChatCtrl', ['$scope', function($scope) {
-  $(document).ready(function() {
-    $('.button-collapse').sideNav({
-      menuWidth: 240, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-
-    $('.tooltipped').tooltip({delay: 50});
-  });
-
+controller('ChatCtrl', ['$scope', '$location', function($scope, $location) {
   $scope.rooms = [];
   $scope.id = userId;
   $scope.username = "";
   $scope.isAnonymous = false;
-
+  $scope.currTab = 0;
 
   socket.on('connect', function() {
     console.log("Logging in with id: " + $scope.id);
@@ -36,6 +28,7 @@ controller('ChatCtrl', ['$scope', function($scope) {
     $scope.username = msg;
   });
 
+  /* Function that is called when user sends a message using the chat box form */
   $scope.sendMessage = function() {
     var loc;
     $('#class-nav > li').each(function(index) {
@@ -76,10 +69,19 @@ controller('ChatCtrl', ['$scope', function($scope) {
     }
     $scope.$apply();
 
-    /* Sets the first pill to active and shows its content if all inactive  */
+    // Sets first room to active if tab is unspecified in URL.
     if(!$('#class-nav .active').length) {
       $('#class-nav li:first').addClass('active');
-      $('.tab-pane:first').addClass('active');
+    }
+
+    // Switches active tab depending on url
+    var url = window.location.href;
+    // Gets tab # at the end of url.
+    // Depends on chat.php .my-tab a href!!!
+    var currentTab = url.split('#/')[1]; 
+    console.log('split url:', currentTab);
+    if (currentTab != undefined && currentTab == parseInt(currentTab, 10)) {
+      $scope.switchActiveTab(currentTab);
     }
   });
 
@@ -100,7 +102,7 @@ controller('ChatCtrl', ['$scope', function($scope) {
       }
     }
     $scope.$apply();
-    $('#message-pane').scrollTop($('#message-pane')[0].scrollHeight);
+    $('.message-pane').scrollTop($('.message-pane')[0].scrollHeight);
     console.log("should have scrolled on message");
   });
 
@@ -115,11 +117,12 @@ controller('ChatCtrl', ['$scope', function($scope) {
   /* Scrolls to the bottom of the chat */
   $scope.scrollDownChat = function(tab) {
     console.log("scroll down chat on tab change...");
-    console.log(tab);
-    $('#message-pane').scrollTop($('#message-pane')[0].scrollHeight);
+    console.log($('.message-pane')[0].scrollHeight);
+    $('.message-pane').scrollTop($('.message-pane')[0].scrollHeight);
     console.log("scroll down chat on tab change DONE");
   };
 
+  /* Leaves the study group that the user is in. */
   $scope.leaveGroup = function(data) {
     console.log("Leaving group: " + data);
     socket.emit('leave group', {"user_id": $scope.id, "group_id": $scope.rooms[data]['group_id']});
@@ -136,15 +139,121 @@ controller('ChatCtrl', ['$scope', function($scope) {
   /* Switches the active tab of the class-nav */
   $scope.switchActiveTab = function(tab) {
     console.log("switch active tab fn", tab);
+    $scope.currTab = tab;
     // Remove active class on all tabs
     $('#class-nav > li').removeClass('active');
 
     // Add active class on pressed tab.
     $($('#class-nav > li')[tab]).addClass('active');
+
+    $scope.scrollDownChat(tab);
   }
 
   /* Determines whether tab is active. */
   $scope.isTabActive = function(index) {
     return $($('#class-nav > li')[index]).hasClass('active');
   }
-}]);
+  
+  /* Opens a dropdown from the sidebar and doesn't apply clicks to parent. */
+  $scope.toggleDropdown = function(group, event) {
+    event.stopPropagation();
+    $scope.openedDropdownGroup = ($scope.openedDropdownGroup == group) ? null : group;
+    console.log("toggle dropdowns", group);
+  };
+
+  $scope.toggleClassInfo = function(group, event) {
+    $scope.openedClassInfo = ($scope.openedClassInfo == group) ? null : group;
+    console.log("toggleclass infos", group);
+  };
+
+  $scope.toggleChatSig = function(index) {
+    $scope.openedChatSig = ($scope.openedChatSig == index) ? null : index;
+  };
+
+  $scope.closeSideNav = function() {
+    console.log('Close the side nav');
+    $('.button-collapse').sideNav('hide');
+  };
+
+  $scope.focusInput = function() {
+    document.getElementById("input--message").focus();
+  }
+
+  $scope.tellMeStuff = function() {
+    console.log("TELL ME STUFF");
+    console.log("$scope.openedDropdownGroup=", $scope.openedDropdownGroup);
+    console.log("$scope.openedClassInfo=", $scope.openedClassInfo);
+  };
+
+  /* Occurs before other things */
+  $(document).ready(function() {
+    // materializecss initialization
+    $('.button-collapse').sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+
+    $('.tooltipped').tooltip({delay: 50});
+
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Doesn't change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false // Displays dropdown below the button
+    });
+
+    $scope.focusInput();
+
+    /* Whole document click listener where you can closing event functions */  
+    $(document).click(function(event) {
+      // Closes the opened .sp-dropdowns on any click outside of it.
+
+/*
+      console.log(event.target);
+
+
+      if ($scope.openedChatSig != null &&
+          event.target !== $('.chat-bubble--open-sig')) {
+        $scope.openedChatSig = null;
+        console.log('CLOSE THIS SIG');
+        $scope.$apply();
+      }
+*/
+
+      if ($scope.openedDropdownGroup != null &&
+          event.target != $('.sp-dropdown--open')) {
+        $scope.openedDropdownGroup = null;
+        console.log('CLOSE THIS STUFF');
+        $scope.$apply();
+      }
+    });
+  });
+}]).
+
+filter('dateify_time', function() {
+  return function(input) {
+    //console.log('dateify time:', input);
+    return moment(input).format('ddd, MM/DD/YYYY, h:mmA');
+  };
+}).
+
+filter('dateify_date', function() {
+  return function(input) {
+    //console.log('dateify date:', input);
+    return moment(input).format('ddd, MM/DD/YYYY');
+  };
+}).
+
+filter('titler', function() {
+  return function(input) {
+    console.log('titler :', input);
+    var currentTab = input.split('#/')[1]; 
+    if (currentTab == 1) {
+      currentTab = 0;
+    }
+    return $scope.rooms[currentTab];
+  };
+});
