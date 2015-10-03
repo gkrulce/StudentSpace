9bd59e15cb8a0323b95f3d859afc71e28a352963
Galen Krulce <gkrulce@ucsd.edu>
2015-10-02 20:01:01 -0700

    Modified database backup script

--- MySQL/getBackup.sh
+++ MySQL/getBackup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 currDate=`date +%Y-%m-%d`
-bakDir="/home/ubuntu/MySQL_backups/"
+bakDir="/home/gkrulce/Dropbox/"
 fileName="StudentSpace_bak_"$currDate.sql
 savePath=$bakDir$fileName
 
-mysqldump -u ggr ucsdspace > $savePath
+mysqldump -u webapp ucsdspace > $savePath
 if [ $? -eq 0 ]; then
   echo "Backup saved to $savePath"
 else
